d2ccdf5d71d1e2fcf398943e5cb68fc52bb8fcb9
muho <hi@muho.dev>
2023-07-28 02:29:38 +0300

    style improvements

--- web/app/(home)/page.tsx
+++ web/app/(home)/page.tsx
@@ -15,17 +15,20 @@ export default async function Home() {
 
   return (
     <main>
-      <OpinionTitle />
-      <FeaturedPost
-        thumbnail={data?.featuredPost?.thumbnail}
-        author={data?.featuredPost?.author}
-        slug={data?.featuredPost?.slug}
-        title={data?.featuredPost?.title}
-      />
-      <HeroPosts
-        topPosts={array(data?.topPosts)}
-        popularPosts={array(data?.popularPosts)}
-      />
+      <div className="h-top__grid">
+        <OpinionTitle />
+        <FeaturedPost
+          thumbnail={data?.featuredPost?.thumbnail}
+          author={data?.featuredPost?.author}
+          slug={data?.featuredPost?.slug}
+          title={data?.featuredPost?.title}
+        />
+        <HeroPosts
+          topPosts={array(data?.topPosts)}
+          popularPosts={array(data?.popularPosts)}
+        />
+      </div>
+
       <div className="h-container h-posts__grid">
         <LatestPosts posts={array(data?.latestPosts)} />
         <PickedPosts posts={array(data?.pickedPosts)} />
