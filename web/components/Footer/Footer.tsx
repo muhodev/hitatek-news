870040b7fd069bdfd5be33e5c71c023c717d1d81
muho <hi@muho.dev>
2023-07-28 02:21:59 +0300

    Style footer

--- web/components/Footer/Footer.tsx
+++ web/components/Footer/Footer.tsx
@@ -1,3 +1,12 @@
 export default function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <footer
+      style={{
+        background: "var(--solid-blue-80, #001733)",
+        width: "100%",
+        height: "80px",
+        marginTop: 100,
+      }}
+    ></footer>
+  );
 }
